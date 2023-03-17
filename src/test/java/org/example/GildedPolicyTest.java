c67c119d6054553b6cb5fc19b4b82341d899a4d8
Eleanor Rhodes <erhodes@policyexpert.co.uk>
2023-03-17 13:26:43 +0000

    Add passing test for percentage always being positive

--- src/test/java/org/example/GildedPolicyTest.java
+++ src/test/java/org/example/GildedPolicyTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +17,17 @@ class GildedPolicyTest {
         app.updatePercentage();
 
         assertEquals(policyName, app.policies[0].name);
+    }
+
+    @Test
+    void percentageCannotBeNegative() {
+        Policy[] policies = new Policy[]{new Policy("Policy Name", 0, 0)};
+        GildedPolicy app = new GildedPolicy(policies);
+
+        app.updatePercentage();
+
+        assertEquals(app.policies[0].percentage, 0);
+
     }
 
 }
